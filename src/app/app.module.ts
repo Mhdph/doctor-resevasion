743e2a7866a53ebe10afebc263618f873f3485d3
mahdi parivash <mahdiparivash@protonmail.com>
2022-08-15 15:58:54 +0430

    make all file in seperate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { HerosectionComponent } from './herosection/herosection.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CarouselModule } from '@coreui/angular';
+import { FooterComponent } from './footer/footer.component';
+import { MainsectionComponent } from './mainsection/mainsection.component';
 @NgModule({
-  declarations: [AppComponent, HerosectionComponent, CarouselComponent],
+  declarations: [AppComponent, HerosectionComponent, CarouselComponent, FooterComponent, MainsectionComponent],
   imports: [BrowserModule, CarouselModule, FontAwesomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/mainsection/mainsection.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainsection',
+  templateUrl: './mainsection.component.html',
+})
+export class MainsectionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
